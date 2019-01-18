fc93b0302fa9f4fd6aa88163289ba424ca8f05d2
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-18 15:35:08 -0800

    add initial files and directories

---
+++ src/objectRepository/RediffLoginPage.java
@@ -0,0 +1,11 @@
+package objectRepository;
+
+import org.openqa.selenium.By;
+
+public class RediffLoginPage {
+	
+	By userNameInput = By.id("login1");
+	By passwordInput = By.id("password");
+	By goButton = By.name("proceed");
+	
+}
